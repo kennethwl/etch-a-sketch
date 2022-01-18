84f54cbfeb18a45fbf382d785fc5ca0d4c518ae7
kennethwl <kenneth.weltens@gmail.com>
2022-01-18 08:35:42 +0100

    Improved button functionallity.

--- js/e-a-s.js
+++ js/e-a-s.js
@@ -1,64 +1,30 @@
 const container = document.querySelector('#container');
-
-
-let size = 16
-let color = 'black'
-window.onload = createGrid(size)
+let size = 16;
+let color = 'black';
+window.onload = createGrid(size);
 
 function createGrid(size){
     container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
    
     for(let i = 0; i < size; i++){
         for(let j =0; j < size; j++){
             const squares = document.createElement('div');
             squares.setAttribute('class', 'square');
-            squares.addEventListener('mouseover', () =>{ squares.style.backgroundColor = color}) 
+            squares.addEventListener('mouseover', () =>{ squares.style.backgroundColor = color});
             container.appendChild(squares);
-
         }
     }
+    getColor();
 }
-/*
-createGrid(size);
-*/
 
 const buttonClear = document.querySelector('.clear-all');
-const boxes = document.querySelectorAll('.square');
-const buttonBlack = document.querySelector('.black');
-const buttonRGB = document.querySelector('.rgb');
-const buttonWhite = document.querySelector('.white')
-
 buttonClear.addEventListener('click', () =>{
     clearGrid();
 })
 
-buttonWhite.addEventListener('click',()=>{
-    boxes.forEach(box => {
-        box.addEventListener('mouseover', ()=>{
-            box.style.backgroundColor = 'white';
-        })
-    })
-})
-
-buttonRGB.addEventListener('click', () => {
-    boxes.forEach(box => {
-        box.addEventListener('mouseover', ()=>{
-            box.style.backgroundColor = randomColor();
-        })
-    })
-   
-})
-
-buttonBlack.addEventListener('click', () =>{
-    boxes.forEach(box => {
-        box.addEventListener('mouseover', ()=>{
-            box.style.backgroundColor ='black';
-        })
-    }) 
-})
-
 function clearGrid(){
+    const boxes = document.querySelectorAll('.square');
     boxes.forEach(box => {box.remove() });
     size = getUserInput();
     createGrid(size);
@@ -75,10 +41,39 @@ function getUserInput(){
     return size;
 }
 
-function randomColor(){
-    let r = () => Math.floor(Math.random() * 256);
-    let color = `rgb(${r()}, ${r()}, ${r()})`
-    return color
+function getColor(){                                        // Checks wich color button is clicked and sets color accordingly.
+    const buttonWhite = document.querySelector('.white');
+    const buttonBlack = document.querySelector('.black');
+    const buttonRGB = document.querySelector('.rgb');
+    const boxes = document.querySelectorAll('.square');
+
+    buttonWhite.addEventListener('click',()=>{
+        boxes.forEach(box => {
+            box.addEventListener('mouseover', ()=>{
+                box.style.backgroundColor = 'white';
+            })
+        })
+    })
+    
+    buttonRGB.addEventListener('click', () => {
+        boxes.forEach(box => {
+            box.addEventListener('mouseover', ()=>{
+                box.style.backgroundColor = getrandomColor();
+            })
+        })
+       
+    })
+    
+    buttonBlack.addEventListener('click', () =>{
+        boxes.forEach(box => {
+            box.addEventListener('mouseover', ()=>{
+                box.style.backgroundColor ='black';
+            })
+        }) 
+    })
 }
-
-
+function getrandomColor(){                      
+    let r = () => Math.floor(Math.random() * 256);
+    let color = `rgb(${r()}, ${r()}, ${r()})`;
+    return color;
+}
